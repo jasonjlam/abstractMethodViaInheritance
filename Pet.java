8cb52ebc957a3b7120a8744dde514168a60ac480
jlam01 <jlam01@stuy.edu>
2019-02-28 14:02:03 -0500

    testing

---
+++ Pet.java
@@ -0,0 +1,5 @@
+public class Pet extends Animal{
+	public String speak() {
+	return "";
+	}
+}
---
+++ UserOfSpeakers.java
@@ -0,0 +1,7 @@
+public class UserOfSpeakers {
+	public static void main (String[] commandLine) {
+		Animal Lucky = new Retriever();
+		System.out.println(Lucky.speak());
+	}
+}
+		
